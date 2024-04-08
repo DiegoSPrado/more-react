b64b6d6bc95032de59ecb5af2d8c132e1f7be5ad
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-04-08 07:39:43 -0300

    criado

---
+++ treino-more/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  function sum(a, b) {
+    return 
+  }
+  
+  return (
+    <div className="App">
+     <h1>Olá React!</h1>
+     <p>Meu primeiro App.</p>
+     <p>Soma: {sum(1 + 2)}</p>
+    </div>
+  );
+}
+
+export default App;
