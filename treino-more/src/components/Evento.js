bdc498d00545583d18086859c92ed5f97ed4eee8
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-04-10 10:14:38 -0300

    a

--- treino-more/src/components/Evento.js
+++ treino-more/src/components/Evento.js
@@ -1,13 +1,14 @@
-function Evento({numero}) {
+function Evento() {
     
     function MeuEvento() {
-        alert(`Você Clicou! ${numero}`)
+        alert(`Você Clicou! `)
     }
     
     return (
         <div>
             <p>Este botão irá disparar um evento em jfodas</p>
             <button onClick={MeuEvento}>CLIQUE AQUI!!!</button>
+            <Button event={meuEvento}/>
         </div>
     )
 }
--- treino-more/src/components/Form.js
+++ treino-more/src/components/Form.js
@@ -1,14 +1,32 @@
+import { useState } from 'react'
+
 function Form() {
     
     function Cadastro(e) {
        e.preventDefault()
-        prompt('Você confirma o seu cadastro?') 
+        console.log(`Cadastro Finalizado com o nome ${name} e a senha: ${password}`) 
     }
     
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
     return (
         <div>
+            <h1>Meu Cadastro</h1>
             <form onSubmit={Cadastro}>
-                <div><input type="text" placeholder="Digite seu nome aqui."></input>
+                <div><label htmlFor="name">Nome: <input type="text" placeholder="Digite seu nome aqui."
+                id="name"
+                name="name" required="on"
+                onChange={(e) => setName(e.target.value)}></input>
+                </label>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: <input type="password" placeholder="Digite a sua senha."
+                    id="password"
+                    name="password" required="on"
+                    onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                    </label>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"></input>
